f5723a5b97c36a63808a2ac0c938ca8078c4b730
John Pywtorak <jpywtora@gmail.com>
2016-05-16 15:32:12 -0700

    Fix the race condition and almost always error between homebrew plugins

--- Dev/Homebrew/brew-updates.1h.sh
+++ Dev/Homebrew/brew-updates.1h.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+
 # <bitbar.title>Homebrew Updates</bitbar.title>
 # <bitbar.author>killercup</bitbar.author>
 # <bitbar.author.github>killercup</bitbar.author.github>
 # <bitbar.desc>List available updates from Homebrew (OS X)</bitbar.desc>
 
+# really should have error checking and retry if locked out by another brew update
 exit_with_error() {
   echo "err | color=red";
   exit 1;
 }
 
-/usr/local/bin/brew update > /dev/null || exit_with_error;
+/usr/local/bin/brew update &> /dev/null || exit_with_error;
 
 UPDATES=$(/usr/local/bin/brew outdated --verbose);
 
@@ -18,6 +20,11 @@ UPDATE_COUNT=$(echo "$UPDATES" | grep -c '[^[:space:]]');
 echo "↑$UPDATE_COUNT | dropdown=false"
 echo "---";
 if [ -n "$UPDATES" ]; then
-  echo "Upgrade all | bash=/usr/local/bin/brew param1=upgrade terminal=false refresh=true"
-  echo "$UPDATES" | awk '{print $0 " | terminal=false refresh=true bash=/usr/local/bin/brew param1=upgrade param2=" $1}'
+  echo "Upgrade all | bash=/usr/local/bin/brew param1=upgrade terminal=true refresh="
+  echo "$UPDATES" | awk '{print $0 " | bash=/usr/local/bin/brew param1=upgrade param2=" $1 " terminal=true refresh="}'
 fi
+echo "---";
+echo "Brew Update | bash=brew param1=update terminal=true refresh="
+#echo "Brew Upgrade | bash=brew param1=upgrade terminal=true refresh="
+echo "Brew Cleanup | bash=brew param1=cleanup terminal=true refresh="
+echo "Refresh | refresh="
--- Dev/Homebrew/homebrewcask.1d.sh
+++ Dev/Homebrew/homebrewcask.1d.sh
@@ -7,13 +7,16 @@
 # <bitbar.desc>List out-of-date apps i.e. apps for whch a newer Homebrew cask exists. Click menu item to install cask.</bitbar.desc>
 # <bitbar.image>http://i.imgur.com/kBf90TB.png?1</bitbar.image>
 
+# No need to perform the brew update, plugin brew-updates does this
+# on a refresh it is a race to lock and I found brew-updates loosing and error
+# so, just let that plugin do the update, it executes more frequently
 #I've taken the next six lines from the brew update plugin.
-exit_with_error() {
-  echo "err | color=red";
-  exit 1;
-}
+# exit_with_error() {
+#   echo "err | color=red";
+#   exit 1;
+# }
 
-/usr/local/bin/brew update &> /dev/null || exit_with_error; 
+#/usr/local/bin/brew update &> /dev/null || exit_with_error;
 
 brewcasklist=$(/usr/local/bin/brew cask ls -1 | sed 's_(!)__g' | xargs /usr/local/bin/brew cask info | grep -A 1 'Not installed' | sed -e 's_Not installed__g' -e 's_https://github\.com/caskroom/homebrew-cask/blob/master/Casks/__g' -e 's_\.rb__g');
 
@@ -32,9 +35,9 @@ fi
 for line in $brewcasklist; do echo "$line" | grep "[a-z]" | sed 's_\(.*\)_& | bash=brew param1=cask param2=install param3=& terminal=true refresh=_g' ; done
 echo "---"
 # Uncomment following lines to add the commands to the drop-down menu
-# echo "Brew Update | bash=brew param1=update terminal=true refresh="
+echo "Brew Update | bash=brew param1=update terminal=true refresh="
 echo "Brew Upgrade | bash=brew param1=upgrade terminal=true refresh="
-# echo "Brew Cleanup | bash=brew param1=cleanup terminal=true refresh="
-# echo "Brew Cask Cleanup | bash=brew param1=cask param2=cleanup terminal=true refresh="
+echo "Brew Cleanup | bash=brew param1=cleanup terminal=true refresh="
+echo "Brew Cask Cleanup | bash=brew param1=cask param2=cleanup terminal=true refresh="
 echo "Refresh | refresh="
 fi
